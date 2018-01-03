9be76e7530f5e449c347cdd2c0d58256d8080761
mzaleskis <zaloogle@gmail.com>
2018-01-03 13:06:56 +0200

    Pridetas prisijungimo langas

---
+++ app/src/main/java/com/byethost32/httpindre1234/darbuotoju_sarasas/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.byethost32.httpindre1234.darbuotoju_sarasas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
